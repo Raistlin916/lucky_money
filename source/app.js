7d98685b2d4c3e72a6265c4b92c289dd5afbd714
gaokai <raistlin916@gmail.com>
2019-02-20 02:20:11 +0800

    add tokens util

--- source/app.js
+++ source/app.js
@@ -1,25 +1,25 @@
-import React from '@react';
-import './pages/index/index';
+import React from "@react";
+import "./pages/index/index";
 class Global extends React.Component {
-    config = {
-        window: {
-            navigationBarBackgroundColor: 'white',
-            backgroundTextStyle: 'light',
-            navigationBarTitleText: '百万大奖等着你',
-            navigationBarTextStyle: 'black'
-        }
-    };
-    onLaunch() {
-        //针对快应用的全局getApp补丁
-        if (this.$data && typeof global === 'object') {
-            var ref = Object.getPrototypeOf(global) || global;
-            var _this = this;
-            ref.getApp = function() {
-                return _this;
-            };
-        }
-        console.log('App launched');//eslint-disable-line
+  config = {
+    window: {
+      navigationBarBackgroundColor: "white",
+      backgroundTextStyle: "light",
+      navigationBarTitleText: "百万大奖等着你",
+      navigationBarTextStyle: "black"
     }
+  };
+  onLaunch() {
+    //针对快应用的全局getApp补丁
+    if (this.$data && typeof global === "object") {
+      var ref = Object.getPrototypeOf(global) || global;
+      var _this = this;
+      ref.getApp = function() {
+        return _this;
+      };
+    }
+    console.log("App launched"); //eslint-disable-line
+  }
 }
 // eslint-disable-next-line
 export default App(new Global());
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -6,10 +6,131 @@ import LuckyCodeList from "@components/LuckyCodeList/index"
 import AdList from "@components/AdList/index"
 import "./index.scss"
 
+const tokens = {
+  storageKey: 'LOGIN_DATA',
+  tokens: {},
+  userInfo: {},
+  login() {
+    let loginData = wx.getStorageSync(this.storageKey)
+    const emptyLoginData = {
+      accessToken: null,
+      refreshToken: null
+    }
+    try {
+      if (loginData) {
+        loginData = {
+          ...emptyLoginData,
+          ...JSON.parse(loginData)
+        }
+      } else {
+        throw new Error()
+      }
+    } catch(e) {
+      loginData = {
+        ...emptyLoginData
+      }
+    }
+    if (!loginData.accessToken) {
+      this.weixinAuth()
+      return
+    }
+
+    Object.assign(this.tokens, loginData)
+
+    wx.checkSession({
+      success: (e) => {
+        this.fetchUserInfo()
+      },
+      fail: (e) => {
+        this.weixinAuth()
+      }
+    })
+  },
+  weixinAuth() {
+    wx.login({
+      success: (r) => {
+        wx.request({
+          url: 'https://api.ippapp.com/uc/auth/wmpAuthorize',
+          method: 'post',
+          data: {
+            client: {clientId: 'wmp_lottery_v1'},
+            authorizationType: 'wechat_micro_program',
+            authDetail: {authorizationCode: r.code}
+          },
+          success: ({ data }) => {
+            Object.assign(this.tokens, data)
+            wx.setStorageSync(this.storageKey, JSON.stringify(this.tokens))
+            this.login()
+          }
+        })
+      },
+      fail(e) {
+        console.log(e)
+      }
+    });
+  },
+  fetchUserInfo() {
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: (res) => {
+              this.saveUserInfo(res)
+            }
+          })
+        } else {
+          console.log('need user auth')
+        }
+      }
+    })
+  },
+  saveUserInfo({ userInfo, ...rest}) {
+    Object.assign(this.userInfo, userInfo)
+    wx.request({
+      url: 'https://api.ippapp.com/uc/wechatUser/syncWmpUserInfo',
+      method: 'post',
+      data: {
+        ...rest
+      },
+      header: {
+        'X-Client-Id': 'wmp_lottery_v1',
+        'X-Access-Token': this.tokens.accessToken
+      },
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  }
+}
+
 class P extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      code: ''
+    }
+  }
+  componentDidMount() {
+    tokens.login()
+  }
+  getUserInfo() {
+    React.api.getUserInfo({
+      success: function(res) {
+        tokens.saveUserInfo(res.userInfo)
+      }
+    });
+  }
   render() {
     return (
       <div class="page">
+        <div>{this.state.code}</div>
+        <button
+					type="primary"
+					open-type="getUserInfo"
+					onGetUserInfo={this.getUserInfo.bind(this)}
+				>
+					授权登录
+				</button>
         <ResultBlock />
         <LuckyCodeList />
         <p className="line">
